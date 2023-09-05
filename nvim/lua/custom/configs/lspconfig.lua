67f3b400d7c29e51bf9dee39f370ee187d5c8557
anon <Hjalte Nannestad>
2023-09-05 03:40:24 +0200

    Merge

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -1,12 +1,14 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
-local util = require "lspconfig/util"
 
 lspconfig.jdtls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    settings = {
+})
 
-    },
+lspconfig.clangd.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+
 })
