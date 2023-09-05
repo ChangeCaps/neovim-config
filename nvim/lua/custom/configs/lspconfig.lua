9a6d0e9f24b2a591a1ad3bf35f9bbe9cc9abb5ad
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2023-09-05 20:01:25 +0200

    Add web language servers

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -2,13 +2,33 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
+-- java
 lspconfig.jdtls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
 })
 
+-- c/c++
 lspconfig.clangd.setup({
   on_attach = on_attach,
   capabilities = capabilities,
 
+})
+
+-- html
+lspconfig.html.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- css
+lspconfig.cssls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- javascript
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
 })
