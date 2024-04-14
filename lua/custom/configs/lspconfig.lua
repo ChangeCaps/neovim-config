2daac1fa6a52904c0ca28aecf36b4a3b5d5a621d
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-04-14 13:15:13 +0200

    Setup nix

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -32,6 +32,12 @@ lspconfig.tsserver.setup({
   capabilities = capabilities,
 })
 
+-- nix
+lspconfig.rnix.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
 -- zig
 lspconfig.zls.setup({
   on_attach = on_attach,
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,9 +1,4 @@
-
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-
+-- Add Run command
 vim.api.nvim_create_user_command(
   "Run",
   function()
