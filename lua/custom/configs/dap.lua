a75fb9e25573f82c4121e0ec79fec65129b23bb2
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-06-24 19:37:35 +0200

    Try fixing gdb

--- lua/custom/configs/dap.lua
+++ lua/custom/configs/dap.lua
@@ -11,12 +11,16 @@ local cxx = {
     type = "gdb",
     request = "launch",
     program = function()
+      -- If we have a Makefile, use it to find the executable
+      if vim.fn.filereadable("Makefile") == 1 then
+        return "make run"
+      end
+
+      -- Otherwise, ask the user for the path to the executable
       return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
     end,
     cwd = "${workspaceFolder}",
-    stopOnEntry = true,
-    args = {},
-    runInTerminal = false,
+    stopAtBeginningOfMainSubprogram = false,
   },
 }
 
