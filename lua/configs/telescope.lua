678393989009fcd7865a0e7268752e5fc13d4636
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-08-06 12:05:21 +0200

    Fix telescope config again

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,8 +1,12 @@
 local M = {}
 
-M.defaults.mappings.i = {
-  ["<C-j>"] = "move_selection_next",
-  ["<C-k>"] = "move_selection_previous",
+M.defaults = {
+  mappings = {
+    i = {
+      ["<C-j>"] = "move_selection_next",
+      ["<C-k>"] = "move_selection_previous",
+    }
+  }
 }
 
 table.insert(M.extensions_list, "ui-select")
